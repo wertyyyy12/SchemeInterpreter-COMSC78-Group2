e87206d981e6e33b786c823bad45baeb8a5bd0f3
Chris <csepka16@gmail.com>
2023-05-03 21:23:19 -0700

    completed problem 1

--- scheme_classes.py
+++ scheme_classes.py
@@ -28,13 +28,13 @@ class Frame:
     def define(self, symbol, value):
         """Define Scheme SYMBOL to have VALUE."""
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
+        self.bindings[symbol] = value
         # END PROBLEM 1
 
     def lookup(self, symbol):
         """Return the value bound to SYMBOL. Errors if SYMBOL is not found."""
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
+        return self.bindings[symbol]
         # END PROBLEM 1
         raise SchemeError('unknown identifier: {0}'.format(symbol))
 
