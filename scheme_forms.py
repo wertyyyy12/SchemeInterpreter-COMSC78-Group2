c4501fa6b25f8672ffaeb37c1cc658171e230fd7
Chris <csepka16@gmail.com>
2023-05-16 21:40:20 -0700

    finished problem 13

--- scheme_forms.py
+++ scheme_forms.py
@@ -162,7 +162,11 @@ def do_cond_form(expressions, env):
         if is_scheme_true(test):
             # BEGIN PROBLEM 13
             "*** YOUR CODE HERE ***"
-            
+            if (clause.rest != nil):
+
+                return eval_all(clause.rest, env)
+            else:
+                return test
             # END PROBLEM 13
         expressions = expressions.rest
 
